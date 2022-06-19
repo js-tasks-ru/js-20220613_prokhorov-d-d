55c492007ba93c2adb451d5775b26a289a6211ab
webtester0 <prokhorov.d.d@yandex.ru>
2022-06-19 14:10:07 +0300

    add 1 tassk

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,18 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param = "asc") {
+  let collator = new Intl.Collator("ru-RU", {
+    sensitivity: "variant",
+    caseFirst: "upper",
+  });
 
+  arr =
+    param === "asc"
+      ? arr.slice().sort(collator.compare)
+      : param === "desc"
+      ? arr.slice().sort(collator.compare).reverse()
+      : undefined;
+
+  return arr;
 }
