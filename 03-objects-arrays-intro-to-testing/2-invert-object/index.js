b985e40221f443984db557bff716c4b4f3d99b56
webtester0 <prokhorov.d.d@yandex.ru>
2022-06-21 00:24:32 +0300

    added 2ed task

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,8 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if (Object.keys(obj).length) {
+    const reverseArr = Object.entries(obj).map((item) => item.reverse());
+    return Object.fromEntries(reverseArr);
+  }
 }
