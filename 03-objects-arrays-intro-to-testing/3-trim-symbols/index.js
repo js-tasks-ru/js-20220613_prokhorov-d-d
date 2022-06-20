baf0419e9ba8bd49590626775ccd193d687c0df7
webtester0 <prokhorov.d.d@yandex.ru>
2022-06-21 01:32:47 +0300

    failed 1 local suit case

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,26 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
+export function trimSymbols(str, size) {
+  if (size !== undefined) {
+    const noRepeatStr = str
+      .split("")
+      .filter((item, index, arr) => item !== arr[index - 1]);
 
+    let counter = {};
+    for (const char of str) {
+      counter[char] = counter.hasOwnProperty(char) ? (counter[char] += 1) : 1;
+    }
+
+    const result = noRepeatStr.map((item) => {
+      if (counter[item] > size) {
+        return item.repeat(size);
+      }
+      return item;
+    });
+
+    return result.join("");
+  } else {
+    return str;
+  }
 }
